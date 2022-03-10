417e42faef1f4f5c4216e1f38500ff8bdb29137f
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-03-10 22:38:31 +0530

    Dialouge message done

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public NPC npc;
+
+    bool isTalking = false;
+
+    float distance;
+    float curResponseTracker = 0;
+
+    public GameObject player;
+    public GameObject dialogueUI;
+
+    public Text npcName;
+    public Text npcDialogueBox;
+    public Text playerResponse;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+   private void FixedUpdate() {
+       
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if(distance <= 2.5f)
+        {
+            if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                curResponseTracker++;
+                if (curResponseTracker >= npc.playerDialogue.Length - 1)
+                {
+                    curResponseTracker = npc.playerDialogue.Length - 1;
+                }
+            }
+            else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                curResponseTracker--;
+                if(curResponseTracker < 0)
+                {
+                    curResponseTracker = 0;
+                }
+            }
+            //trigger dialogue
+            if(Input.GetKeyDown(KeyCode.E) && isTalking==false)
+            {
+                StartConversation();
+            }
+            else if(Input.GetKeyDown(KeyCode.E) && isTalking == true)
+            {
+                EndDialogue();
+            }
+
+            if(curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+                if(Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[1];
+                }
+            }
+            else if(curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
+            {
+                playerResponse.text = npc.playerDialogue[1];
+                if(Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[2];
+                }
+            }
+            else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
+            {
+                playerResponse.text = npc.playerDialogue[2];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[3];
+                }
+            }
+
+        }
+    }
+
+    void StartConversation()
+    {
+        isTalking = true;
+        curResponseTracker = 0;
+        dialogueUI.SetActive(true);
+        npcName.text = npc.name;
+        npcDialogueBox.text = npc.dialogue[0];
+    }
+
+    void EndDialogue()
+    {
+        isTalking = false;
+        dialogueUI.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC file", menuName = "NPC Files Archive")]
+public class NPC : ScriptableObject
+{
+    public string name;
+    [TextArea(3, 15)]
+    public string[] dialogue;
+    [TextArea(3, 15)]
+    public string[] playerDialogue;
+}
